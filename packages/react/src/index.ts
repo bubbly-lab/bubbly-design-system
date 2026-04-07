d4dfc6f41ed69c5b9ecb55a1738243117668d3a7
ndaemy <yuyaebean@gmail.com>
2026-04-07 19:59:59 +0900

    feat: IconButton recipe 생성 및 panda.config.ts 등록

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,6 @@
 export { Button, type ButtonProps } from './components/button';
 export { Divider, type DividerProps } from './components/divider';
+export { IconButton, type IconButtonProps } from './components/icon-button';
 export type {
   ItemContentProps,
   ItemProps,
