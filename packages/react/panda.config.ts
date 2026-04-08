da350fceed53d0a266a8c540826a5cf1c0fd8866
ndaemy <yuyaebean@gmail.com>
2026-04-09 02:11:36 +0900

    feat(react): TextButton 컴포넌트 구현

--- packages/react/panda.config.ts
+++ packages/react/panda.config.ts
@@ -7,6 +7,7 @@ import {
 import { buttonRecipe } from './src/recipes/button';
 import { dividerRecipe } from './src/recipes/divider';
 import { tabsRecipe } from './src/recipes/tabs';
+import { textButtonRecipe } from './src/recipes/text-button';
 
 export default defineConfig({
   preflight: false,
@@ -27,6 +28,7 @@ export default defineConfig({
       button: buttonRecipe,
       divider: dividerRecipe,
       tabs: tabsRecipe,
+      textButton: textButtonRecipe,
     },
   },
 });
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -9,3 +9,4 @@ export type {
   TabsTriggerProps,
 } from './components/tabs';
 export { Tabs } from './components/tabs';
+export { TextButton, type TextButtonProps } from './components/text-button';
