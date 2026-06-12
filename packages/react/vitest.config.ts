ef607ff891280cd201ab4f73ca0db6df9baa8f46
ndaemy <yuyaebean@gmail.com>
2026-06-12 17:32:38 +0900

    chore(react): addon-vitest가 자동 적용하는 vitest setup 파일 제거

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -29,7 +29,6 @@ export default defineConfig({
             headless: true,
             instances: [{ browser: 'chromium' }],
           },
-          setupFiles: ['./.storybook/vitest.setup.ts'],
         },
       },
     ],
